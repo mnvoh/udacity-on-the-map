0b01955a9af0a2f9218b93a432fb413fe5e5dfcd
Milad Nozari <mnvoh90@gmail.com>
2017-03-14 23:02:39 +0300

    feat: Created most of the UI in the storyboard

---
+++ OnTheMap/Controllers/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by Milad Nozari on 3/14/17.
+//  Copyright © 2017 Nozary. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
