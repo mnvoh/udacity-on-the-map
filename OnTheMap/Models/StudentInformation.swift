6ae9a06a89ab572b9cb465c421ad278a1f128bfc
Milad Nozari <mnvoh90@gmail.com>
2017-03-15 19:20:25 +0300

    feat: Created the StudenInformation model

---
+++ OnTheMap/Models/StudentInformation.swift
@@ -0,0 +1,20 @@
+//
+//  StudentInformation.swift
+//  OnTheMap
+//
+//  Created by Milad Nozari on 3/15/17.
+//  Copyright © 2017 Nozary. All rights reserved.
+//
+
+import Foundation
+
+struct StudentInformation {
+    let objectId: String
+    let uniqueKey: String
+    let firstName: String
+    let lastName: String
+    let mapString: String
+    let mediaUrl: String
+    let latitude: Double
+    let longitude: Double
+}
